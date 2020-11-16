62d737a0a4559f7fd111ecefd817829d710ba67f
csx <826555671@qq.com>
2020-11-16 08:27:02 +0800

    增加输入模块

---
+++ Assets/Scripts/Framework/Input/InputMgr.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputMgr : SingletonBase<InputMgr>
+{
+    private bool _isStart = false;
+    public InputMgr()
+    {
+        MonoMgr.Instance.AddUpdateListener(Update);
+    }
+
+    public void StartOrEndCheck(bool isOpen)
+    {
+        _isStart = isOpen;
+    }
+
+    private void CheckKeyCode(KeyCode key)
+    {
+        if (Input.GetKeyDown(key))
+        {
+            EventCenter.Instance.EventTrigger("keydown", key);
+        }
+
+        if (Input.GetKeyUp(key))
+        {
+            EventCenter.Instance.EventTrigger("keyup", key);
+        }
+    }
+    
+    void Update()
+    {
+        if (!_isStart) return;
+        
+        CheckKeyCode(KeyCode.W);
+        CheckKeyCode(KeyCode.S);
+        CheckKeyCode(KeyCode.A);
+        CheckKeyCode(KeyCode.D);
+    }
+}
